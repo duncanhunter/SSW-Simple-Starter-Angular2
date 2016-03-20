6192befb8bade481ff6a86c4667ba30524a35d8d
Duncan Hunter <duncan@duncanhunter.com.au>
2016-03-20 21:21:23 +1100

    todo app initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import { TodoComponent } from './../todo/todo.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<todo></todo>',
+    directives: [TodoComponent]
 })
 export class AppComponent { }
